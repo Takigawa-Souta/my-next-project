c4f86bcf613f9b15b6f7bf7fabda20da2f66bca5
ktc <ktc@503-04.local>
2025-10-18 12:40:59 +0900

    4-2-4made

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,15 +1,10 @@
 import Image from "next/image";
-import styles from "./index.module.css";
+import Link from "next/link";
 
-type News = {
-    id: string;
-    title: string;
-    category: {
-        name: string;
-    };
-    publishedAt: string;
-    createdAt: string;
-};
+import styles from "./index.module.css";
+import Category from "../Category";
+import Date from "../Date";
+import { News } from "@/app/_libs/microcms";
 
 type Props = {
     news: News[];
@@ -23,7 +18,7 @@ export default function NewsList({news}: Props) {
     <ul>
       {news.map((article) => (
         <li key={article.id} className={styles.list}>
-          <div className={styles.link}>
+          <Link href={`/news/${article.id}`} className={styles.link}>
             <Image
               className={styles.image}
               src="/no-image.png"
@@ -34,20 +29,11 @@ export default function NewsList({news}: Props) {
             <dl className={styles.content}>
               <dt className={styles.title}>{article.title}</dt>
               <dd className={styles.meta}>
-                <span className={styles.tag}>{article.category.name}</span>
-                <span className={styles.data}>
-                  <Image
-                    src="/clock.svg"
-                    alt=""
-                    width={16}
-                    height={16}
-                    priority
-                  />
-                  {article.publishedAt}
-                </span>
+                <Category category={article.category} />
+                <Date date={article.publishedAt ?? article.createdAt} />
               </dd>
             </dl>
-          </div>
+          </Link>
         </li>
       ))}
     </ul>
