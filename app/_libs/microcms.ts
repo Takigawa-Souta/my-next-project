4c5d8701b6c10b3351995e93af233a95889cb99c
ktc <ktc@503-04.local>
2025-10-25 12:33:43 +0900

    7^1-2made

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -14,17 +14,15 @@ export type Member = {
 
 export type Category = {
     name: string;
-}
+} & MicroCMSListContent;
 
 export type News = {
-    id: string;
     title: string;
-    category: {
-        name: string;
-    };
-    publishedAt: string;
-    createdAt: string;
-};
+    description: string;
+    content: string;
+    thumbnail: MicroCMSImage;
+    category: Category;
+} & MicroCMSListContent;
 
 if (!process.env.MICROCMS_SERVICE_DOMAIN) {
     throw new Error("MICROCMS_SERVICE_DOMAIN is required");
@@ -46,4 +44,25 @@ export const getMembersList = async (queries?: MicroCMSQueries) => {
             queries,
         });
     return listData;
+};
+
+export const getNewsList = async (queries?: MicroCMSQueries) => {
+    const listData = await client
+        .getList<News>({
+            endpoint: "news",
+            queries,
+        });
+    return listData;
+};
+
+export const getNewsDetail = async (
+    contentId: string,
+    queries?: MicroCMSQueries
+) => {
+    const detailData = await client.getListDetail<News>({
+        endpoint: "news",
+        contentId,
+        queries,
+    });
+    return detailData;
 };
