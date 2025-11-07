65142d7face1f5561824c040ba50d3e566eb534c
Takigawa <ktc25a31g0008@edu.kyoto-tech.ac.jp>
2025-11-08 03:21:05 +0900

    完成

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,6 @@
 import ContactForm from "@/app/_components/ContactForm";
 import styles from "./page.module.css";
 
-
 export default function Page() {
     return (
         <div className={styles.container}>
